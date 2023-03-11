5aef361118ef98195296a61fdf5ceca954130828
Komal Gilani <komalsyeda29@gmail.com>
2023-03-11 11:10:05 +0500

    Initial Commit

---
+++ MyFirstIoSApp/MyFirstIoSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstIoSAppApp.swift
+//  MyFirstIoSApp
+//
+//  Created by Komal Gilani on 11/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstIoSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
